34a9666650b15ff00ca7543a4374c8d901d2f02c
ankitmishra1201 <workwithankit12@gmail.com>
2022-03-04 00:41:37 +0530

    Initial Commit

---
+++ twitter_clone_revelsios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twitter_clone_revelsios
+//
+//  Created by Ankit Mishra on 04/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
